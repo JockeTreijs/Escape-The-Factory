7bd27ba7fc7b0f01f389e8e2b8cc1d152ea072f0
JockeTreijs <jocke.treijs@student.forsbergsskola.se>
2020-10-02 11:27:53 +0200

    Placed drones and leverDoubleDoor
    placed drones with there coherent movement structure.
    Code and placed LeverDoubleOpen and DoorDoubleOpen in that section of the level.

---
+++ Assets/Scripts/LeverDoubleOpen.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverDoubleOpen : MonoBehaviour
+{
+    public DoorDoubleOpen doorDoubleOpen;
+    public bool leverUsed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        doorDoubleOpen = GameObject.Find("DoorDoubleOpen").GetComponent<DoorDoubleOpen>();
+        leverUsed = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (Input.GetKeyDown(KeyCode.E) && leverUsed == false)
+            {
+                leverUsed = true;
+                doorDoubleOpen.OpenDoor();
+            }
+        }
+    }
+
+
+}
