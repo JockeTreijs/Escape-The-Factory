14a1a518124280c5462b965126ea9ddaccb4879f
JockeTreijs <jocke.treijs@student.forsbergsskola.se>
2020-10-06 14:01:04 +0200

    Added playerCross, gameover and max 3 health
    AIm for the player in middle of screen. When healt is 0, the game scene loads again. When health is 3, player cant drink from oilcan

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,26 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public Text healthText;
+    public Player player;
 
     // Start is called before the first frame update
     void Start()
     {
         healthText.text = "Health 3";
+        player = GameObject.Find("Player").GetComponent<Player>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        GameOver();
     }
 
     public void UpdateHealth(int playerHealth)
     {
         healthText.text = "Health " + playerHealth.ToString();
+    }
+
+    public void GameOver()
+    {
+        if (player.health <= 0)
+        {
+            SceneManager.LoadScene("Game");
+        }
     }
 }
