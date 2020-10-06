fc6f6b4044655862981a62be7705d15429b010c0
JockeTreijs <jocke.treijs@student.forsbergsskola.se>
2020-10-06 15:53:28 +0200

    Removed mouse cursor in game mode

--- Assets/Scripts/ThrowableOilcan.cs
+++ Assets/Scripts/ThrowableOilcan.cs
@@ -28,7 +28,7 @@ public class ThrowableOilcan : MonoBehaviour
     }
     IEnumerator DestroyOilcanRoutine()
     {
-        yield return new WaitForSeconds(5.0f);
+        yield return new WaitForSeconds(3.0f);
         Destroy(this.gameObject);
     }
 }
