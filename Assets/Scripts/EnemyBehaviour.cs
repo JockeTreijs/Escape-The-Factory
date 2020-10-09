8671dd09bf8b4a21a42605d7bac3b6dccd4cfbea
mista makzim <56073641+ZaroTV@users.noreply.github.com>
2020-10-09 04:58:50 +0200

    Made level 2
    Created level 2 pretty much, there is one problem with it and it's related to code

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -32,12 +32,15 @@ public class EnemyBehaviour : MonoBehaviour
 
     public Light faceLight;
     public Light faceRoundLight;
+    
 
     private bool coroutineActive = false;
 
     public AudioSource lockOn;
     public ParticleSystem leftZap;
     public ParticleSystem rightZap;
+
+    public int minHeight = 0;
 
     public void Start()
     {
@@ -82,7 +85,7 @@ public class EnemyBehaviour : MonoBehaviour
             canAttack = false;
             //INSERT ZAP ANIMATION HERE
         }
-        transform.position = new Vector3(transform.position.x, 0, transform.position.z);
+        transform.position = new Vector3(transform.position.x, minHeight, transform.position.z);
     }
     private void OnTriggerEnter(Collider other)
     {
